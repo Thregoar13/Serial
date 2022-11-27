12d9ce9b8eaa3bdb893c9f2b5cd99ac349e63521
Peter Sarich <scar.h13@gmail.com>
2022-11-26 17:04:25 -0800

    end of video 2

--- Serial/MainPage.xaml.cs
+++ Serial/MainPage.xaml.cs
@@ -2,23 +2,27 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void btnOpenClose_Clicked(object sender, EventArgs e)
 	{
-		count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
+    private void btnClear_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnSend_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+
 }
 
