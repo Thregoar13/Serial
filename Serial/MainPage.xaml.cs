31f7b306ca5407f44926bf7310126c2e9cd51f8c
Peter Sarich <scar.h13@gmail.com>
2022-11-26 18:56:57 -0800

    end of video 4

--- Serial/MainPage.xaml.cs
+++ Serial/MainPage.xaml.cs
@@ -37,6 +37,50 @@ public partial class MainPage : ContentPage
 	private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
 	{
 		newPacket = serialPort.ReadLine();
+		MainThread.BeginInvokeOnMainThread(MyMainThreadCode);				
+		
+	}
+
+	private void MyMainThreadCode()
+	{
+		if (checkBoxHistory.IsChecked == true)
+		{
+			labelRxdata.Text = newPacket + labelRxdata.Text;
+		}
+		else
+		{
+			labelRxdata.Text = newPacket;
+		}
+		int calChkSum = 0;
+		if (newPacket.Length > 37)
+		{
+			if (newPacket.Substring(0, 3) == "###")
+			{
+				string parsedData = $"{newPacket.Length,-13}" +
+									$"{newPacket.Substring(0, 3),-14}" +
+									$"{newPacket.Substring(3, 3),-14}" +
+									$"{newPacket.Substring(6, 4),-14}" +
+									$"{newPacket.Substring(10, 4),-14}" +
+									$"{newPacket.Substring(14, 4),-14}" +
+									$"{newPacket.Substring(18, 4),-14}" +
+									$"{newPacket.Substring(22, 4),-14}" +
+									$"{newPacket.Substring(26, 4),-14}" +
+									$"{newPacket.Substring(30, 4),-14}" +
+									$"{newPacket.Substring(34, 4),-14}" + "\r\n";
+
+
+
+
+				if (checkBoxParsedHistory.IsChecked == true)
+				{
+					labelParsedData.Text = parsedData + labelParsedData.Text;
+				}
+				else
+				{
+					labelParsedData.Text = parsedData;
+				}
+			}
+        }
 	}
 
 	private void btnOpenClose_Clicked(object sender, EventArgs e)
